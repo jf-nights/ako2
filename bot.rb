a00396c1161b9ad47b771f54fa90038401fbd067
jf712@ring <jf712@kmc.gr.jp>
2017-01-03 20:37:36 +0900

    tweet hoge で @jf_nights で呟く(DMは弾く(utgwくんに感謝

--- bot.rb
+++ bot.rb
@@ -1,4 +1,15 @@
+require 'pp'
+require 'twitter'
 require 'slack-ruby-client'
+
+# Twitterぶぶん
+twitter_token = open("/home/jf712/.twitter/jf_nights").read.split("\n")
+rest_client = Twitter::REST::Client.new do |c|
+  c.consumer_key = twitter_token[0]
+  c.consumer_secret = twitter_token[1]
+  c.access_token = twitter_token[2]
+  c.access_token_secret = twitter_token[3]
+end
 
 # 見ちゃダメよ
 TOKEN = open("/home/jf712/.slack/ako").read.split("\n")[1]
@@ -11,12 +22,31 @@ client.on :hello do
 end
 
 client.on :message do |data|
-  if data.text == "testtt"
+  pp data
+  if data.text == "おはようございます"
     params = {
       channel: "C03ANSF4X",
-      text: "aaa",
+      text: "おはようございます"
     }
     client.message(params)
+  elsif data.text =~ /^tweet (.*)/
+    tweet_text = $1
+    # DM を弾きます
+    if tweet_text =~ /^[dD][mM]? .*/ || tweet_text =~ /[mM] @.*/
+      puts "DIRECT MESSAGE"
+       params = {
+        channel: "C03ANSF4X",
+        text: "ダイレクトメッセージはダ(イレクト)メ(ッセージ)です。"
+      }
+      client.message(params)
+    else
+      rest_client.update(tweet_text + " from #reiankyo")
+      params = {
+        channel: "C03ANSF4X",
+        text: "呟きました。(いい感じについーとのURLを貼り付けたい)"
+      }
+      client.message(params)
+    end
   end
 
 end
