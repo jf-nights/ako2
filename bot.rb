c7abbadb0e4b9777c5ac990f40f4a92673350cdc
jf712@ring <jf712@kmc.gr.jp>
2017-01-07 23:50:08 +0900

    docomo の知識Q&A APIを追加
    そのうち参考URLとか出したい

--- bot.rb
+++ bot.rb
@@ -15,6 +15,15 @@ end
 # 見ちゃダメよ
 TOKEN = open("/home/jf712/.slack/ako").read.split("\n")[1]
 
+# Slackに投稿する時のparam(channel を書くのが面倒だった)
+def makeParam(text, channel="C03ANSF4X")
+  params = {
+    channel: channel,
+    text: text
+  }
+  return params
+end
+
 Slack.configure {|c| c.token = TOKEN}
 client = Slack::RealTime::Client.new
 
@@ -24,6 +33,9 @@ end
 
 client.on :message do |data|
   pp data
+  # docomoAPI用のcontext
+  context = ""
+
   if data.text == "おはようございます"
     params = {
       channel: "C03ANSF4X",
@@ -31,25 +43,38 @@ client.on :message do |data|
     }
     client.message(params)
   elsif data.text =~ /^tweet (.*)/
+    # /^tweet (.*)/ で $1 をメッセージとしてツイート
     tweet_text = $1
     # DM を弾きます
     if tweet_text =~ /^[dD][mM]? .*/ || tweet_text =~ /[mM] @.*/
-      puts "DIRECT MESSAGE"
-       params = {
-        channel: "C03ANSF4X",
-        text: "ダイレクトメッセージはダ(イレクト)メ(ッセージ)です。"
-      }
+      params = makeParam("ダイレクトメッセージはダ(イレクト)メ(ッセージ)です。")
       client.message(params)
     else
       result = rest_client.update(tweet_text + " from #reiankyo")
-      params = {
-        channel: "C03ANSF4X",
-        text: "呟きました。https://twitter.com/jf_nights/status/#{result.id}"
-      }
+      params = makeParam("呟きました。https://twitter.com/jf_nights/status/#{result.id}")
       client.message(params)
     end
-  end
+  elsif data.text =~ /^[dｄ] (.*)/
+    # docomo の雑談API
+    # /^[d|ｄ] (.*)/ で $1 をメッセージとする
+    text = $1
+    response = DocomoAPI.post(text, context)
 
+    if response["requestError"] == nil
+      message = response["utt"] + " [d]"
+      context = response["context"]
+      client.message(makeParam(message))
+    else
+      # error!
+      client.message(makeParam(response))
+    end
+  elsif data.text =~ /^[qQ][aA] (.*)/
+    # docomo の知識Q&A API
+    # /^[qQ][aA] (.*) で $1 を質問内容とする
+    text = $1
+    response = DocomoAPI.postQA(text)
+    client.message(makeParam(response + " [Q&A]"))
+  end
 end
 
 client.start!
--- lib/docomo.rb
+++ lib/docomo.rb
@@ -1,12 +1,15 @@
 require 'net/https'
+require 'cgi'
 require 'json'
+require 'addressable/uri'
 
-module DocomoDialog
-  BASE_URL = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?'
-  api_key = open("/home/jf712/.docomo/ako").read.split("\n")[0]
+module DocomoAPI
+  DIALOGE_URL = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?'
+  QA_URL = 'https://api.apigw.smt.docomo.ne.jp/knowledgeQA/v1/ask?'
+  API_KEY = open("/home/jf712/.docomo/ako").read.split("\n")[0]
 
-  def Post(message, context="", nickname="", nickname_v="", sex="", bloodtype="", birthdateY="", birthdateM="", birthdateD="", age="", constellations="", mode="", t="")
-    uri = URI.parse(BASE_URL + "APIKEY=" + api_key)
+  def post(message, context="", nickname="", nickname_v="", sex="", bloodtype="", birthdateY="", birthdateM="", birthdateD="", age="", constellations="", mode="", t="")
+    uri = URI.parse(DIALOGE_URL + "APIKEY=" + API_KEY)
     https = Net::HTTP.new(uri.host, uri.port)
 
     https.use_ssl = true
@@ -29,10 +32,38 @@ module DocomoDialog
       "t" => t 
     }
 
+    # jsonにしとかないと怒られる
     req.body = data.to_json
     res = https.request(req)
+
+    body = JSON.parse(res.body)
+
+    if res.code == "200"
+      return body
+    else
+      return res.body
+    end
   end
 
-  module_function :Post
-end
+  def postQA(message)
+    uri = Addressable::URI.parse(QA_URL + "q=" + CGI.escape(message) + "&APIKEY=" + API_KEY)
+    res = JSON.parse(Net::HTTP.get(uri))
+    puts res
 
+    if res["code"] =~ /^S.*/
+      # 回答アリ
+      return res["message"]["textForDisplay"]
+    else
+      if res["requestError"] == nil
+        # 回答ナシ
+        return res["message"]
+      else
+        # そもそもエラー
+        return res
+      end
+    end
+
+  end
+
+  module_function :post, :postQA
+end
