e60db1f899ce8ca8464451ba8d0a1dedfca341a6
jf712@ring <jf712@kmc.gr.jp>
2018-01-01 08:29:23 +0900

    来年からあけおめ

---
+++ reminder/akeome.rb
@@ -0,0 +1,7 @@
+# たんじょうび！！！
+require 'json'
+require_relative './post_to_slack'
+
+SlackLib.postMessage("あけましておめでとうございます")
+SlackLib.postMessage("今年もどうぞ、よろしくお願いします")
+
--- reminder/birthday.rb
+++ reminder/birthday.rb
@@ -2,5 +2,6 @@
 require 'json'
 require_relative './post_to_slack'
 
-SlackLib.postMessage("誕生日おめでとうございます!!!!")
+SlackLib.postMessage("@jf712 誕生日おめでとうございます!!!!")
+SlackLib.postMessage("ケーキたくさん食べますよ！")
 
