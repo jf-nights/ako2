3e9c764dbf1ab901cc40c4afa2eb7f487adb2922
jf712@ring <jf712@kmc.gr.jp>
2018-04-27 11:21:02 +0900

    1%の確立でdata.textとはなんですか

--- ako.rb
+++ ako.rb
@@ -26,8 +26,19 @@ class Ako
   #   "team"=>"T0321RSJ5"
   # }
   def recieve(data)
+    # 発言すべき内容とチャンネルの入れもの
+    param = nil
+
+    # ---------------------------
+    # 「おやすみなさい」
+    # ---------------------------
     if data.text == "おやすみなさい"
-      @slack_client.message(makeParam("おやすみなさいませ、<@#{data.user}>さま"))
+      param = makeParam("おやすみなさいませ、<@#{data.user}>さま")
+    end
+
+    case rand(100)
+    when 99
+      param = makeParam("#{data.text}とはなんですか？")
     end
 
     # ---------------------------
@@ -35,9 +46,14 @@ class Ako
     # ---------------------------
     if data.text =~ /^[dｄ] (.*)/
       resp = @responder_DocomoAPI.response($1, @context)
-      @slack_client.message(makeParam(resp["message"]))
+      param = makeParam(resp["message"])
       @context = resp["context"] if resp["context"] != nil
       puts @context
     end
+
+    # --------------------------
+    # paramに発言内容とチャンネルが入っているので
+    # 投稿
+    @slack_client.message(param) if param != nil
   end
 end
