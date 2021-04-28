31df1ed317674bdc0427c5389facb3e406bb3f16
jf712@ring <jf712@kmc.gr.jp>
2021-04-28 15:42:58 +0900

    各月の内容を見れるように

--- sinatr_ako.rb
+++ sinatr_ako.rb
@@ -1,7 +1,24 @@
 Bundler.require
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 require_relative './sinatra/config'
+
+SQLITE = "/home/jf712/projects/ako2/data/db/goldpoint.sqlite3"
+set :database, {adapter: "sqlite3", database: SQLITE}
+
+class Goldpoint < ActiveRecord::Base
+end
 
 get "/" do
   "hello~~~~"
 end
+
+get "/:year/:month" do |year, month|
+  @year = year
+  @month = month
+  @result = Goldpoint.where("date like ?", "%#{year}/#{month}%")
+  pp @result.all
+  
+  erb :yearmonth
+end
+
