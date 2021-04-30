01334ee31359113b09a4f875b202890324c3d556
jf712@ring <jf712@kmc.gr.jp>
2021-04-30 21:17:37 +0900

    カテゴリの内容登録, IDからの編集からカテゴリ登録も

--- sinatr_ako.rb
+++ sinatr_ako.rb
@@ -6,7 +6,13 @@ require_relative './sinatra/config'
 SQLITE = "/home/jf712/projects/ako2/data/db/goldpoint.sqlite3"
 set :database, {adapter: "sqlite3", database: SQLITE}
 
+# 明細を保存するtable
 class Goldpoint < ActiveRecord::Base
+end
+
+# カテゴリを保存するtable
+class Category < ActiveRecord::Base
+  self.table_name = "category"
 end
 
 get "/" do
@@ -28,7 +34,7 @@ end
 
 # /edit/:id へのアクセス
 get %r{/edit/([\d]+)} do |id|
- pp @result = Goldpoint.find_by_id(id)
+ @result = Goldpoint.find_by_id(id)
  if @result == nil
    "idが#{params[:id]}のデータなし！" 
  else
@@ -38,18 +44,65 @@ get %r{/edit/([\d]+)} do |id|
  end
 end
 
+# /edit/:id からid の内容を登録するときに呼ばれる
 post "/regist/:id" do
-  pp params
   id = params[:id]
   data = Goldpoint.find_by_id(id)
   if data == nil
-    puts "nil~~~~~~~~~~~~~~~~~~~~~~~~"
+    session[:msg] = "記録失敗！"
   else
-    # DBへの登録
+    # Goldpointへの登録
     data.category1 = params[:category1]
     data.category2 = params[:category2]
     data.save
-    puts "regist~~~~~~~~~~~~~~~~~~~~~~~"
+
+    # Categoryへの登録
+    category = Category.new
+    category.payee = data.payee
+    category.category1 = params[:category1]
+    category.category2 = params[:category2]
+    category.save
+
+    session[:msg] = "記録成功！"
   end
   redirect to("/edit/#{id}")
+end
+
+# 支払い先，分類1, 分類2の一覧
+get "/categories" do
+  @result = Category.all
+  @msg = session[:msg]
+  erb :categories
+end
+
+# 支払い先，分類1，分類2を登録するときに呼ばれる
+post "/registCategory" do
+  payee = params[:payee]
+  c1 = params[:category1]
+  c2 = params[:category2]
+  @msg = nil
+  if payee.present? && c1.present? && !c2.nil?
+    data = Category.new
+    data.payee = payee
+    data.category1 = c1
+    data.category2 = c2
+    data.save
+    session[:msg] = "保存成功！"
+  else
+    session[:msg] = "保存失敗！"
+  end
+  redirect "/categories"
+end
+
+# 登録済みの支払い先，分類1などを削除する
+post "/deleteCategory" do
+  id = params[:id]
+  data = Category.find_by_id(id)
+  if data == nil
+    session[:msg] = "削除失敗！"
+  else
+    data.destroy
+    session[:msg] = "削除成功！"
+  end
+  redirect "/categories"
 end
--- sinatra/config.rb
+++ sinatra/config.rb
@@ -2,4 +2,5 @@
 set :port, 12321
 # bind address
 set :bind, '0.0.0.0'
-
+# session
+enable :sessions
